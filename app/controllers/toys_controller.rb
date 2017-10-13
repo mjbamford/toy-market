a3742547c34cc7c9187978a96f2d0457cb6a3e07
Michael Bamford <michael@givvtechnologies.com>
2017-10-13 20:11:52 +1100

    Add ToysController#create

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -6,4 +6,14 @@ class ToysController < ApplicationController
   def new
     @toy = Toy.new
   end
+
+  def create
+    respond_to do |format|
+      if toy = (Toy.create params[:toy])
+        format.html { redirect_to toys_path, notice: 'Your toy was successful created' }
+      else
+        format.html { render 'new' }
+      end
+    end
+  end
 end
--- spec/controllers/toys_controller_spec.rb
+++ spec/controllers/toys_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe ToysController, type: :controller do
   render_views
 
-  describe "GET #index" do
+  describe 'GET #index' do
     let!(:toys) { create_list :toy, 3 }
 
-    it "shows all toys" do
+    it 'shows all toys' do
       get :index
       expect(response).to be_successful
       expect(response.body).to match /Toys/
@@ -15,9 +15,16 @@ RSpec.describe ToysController, type: :controller do
   end
 
   describe "GET #new" do
-    it "shows new toy form" do
+    it 'shows new toy form' do
       get :new
       expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new toy' do
+      toy_attrs = attributes_for :toy
+      expect{ post :create, params: toy_attrs }.to change{ Toy.count }.by(1)
     end
   end
 end
