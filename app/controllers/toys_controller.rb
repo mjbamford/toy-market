56be4cfed46e8b2af3e5ecf6e39e7644f97e06a1
Michael Bamford <michael@givvtechnologies.com>
2017-10-14 12:23:51 +1100

    Add presence validation on toy#name

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,19 +1,38 @@
 class ToysController < ApplicationController
+  TOY_PERMITTED_PARAMS = %i[ name description price ]
+
+  rescue_from ActionController::ParameterMissing do
+    @toy = new_toy
+    render :new
+  end
+
   def index
     @toys = Toy.all
   end
 
   def new
-    @toy = Toy.new
+    @toy = new_toy
   end
 
   def create
     respond_to do |format|
-      if toy = (Toy.create params[:toy])
-        format.html { redirect_to toys_path, notice: 'Your toy was successful created' }
+      if (@toy = new_toy toy_params).save
+        format.html { redirect_to toys_path, notice: 'Toy was successfully created.' }
+        format.json { render :show, status: :created, location: @toy }
       else
-        format.html { render 'new' }
+        format.html { render :new }
+        format.json { render json: @toy.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  private
+
+  def toy_params
+    params.require(:toy).permit TOY_PERMITTED_PARAMS
+  end
+
+  def new_toy attrs = {}
+    Toy.new attrs
   end
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,4 +1,5 @@
 class Toy < ApplicationRecord
+  validates :name, presence: true
   # belongs_to :seller, class: 'User'
   # belongs_to :buyer, class: 'User'
 end
