daf291da45510253c019f2751084f388f1b31c21
Michael Bamford <michael@givvtechnologies.com>
2017-10-14 12:49:32 +1100

    Add ToysController#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :toys, only: %i[ index new create ]
+  resources :toys, only: %i[ index show new create ]
   root to: 'homes#show', defaults: { name: 'welcome' }
 end
--- spec/controllers/toys_controller_spec.rb
+++ spec/controllers/toys_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe ToysController, type: :controller do
   render_views
 
+  let(:valid_attrs) { attributes_for :toy }
+  let(:invalid_attrs) {{ name: nil }}
+
   describe 'GET #index' do
     let!(:toys) { create_list :toy, 3 }
 
@@ -21,19 +24,24 @@ RSpec.describe ToysController, type: :controller do
     end
   end
 
-  describe 'POST #create' do
-    let(:valid_attrs) {{ toy: attributes_for(:toy) }}
-    let(:invalid_attrs) {{ toy: { name: nil }}}
+  describe "GET #show" do
+    it "returns a success response" do
+      toy = Toy.create! valid_attrs
+      get :show, params: { id: toy.to_param }
+      expect(response).to be_successful
+    end
+  end
 
+  describe 'POST #create' do
     context 'with valid params' do
       it 'creates a new toy' do
         expect{
-          post :create, params: valid_attrs
+          post :create, params: { toy: valid_attrs }
         }.to change{ Toy.count }.by(1)
       end
 
       it 'redirects to the index page' do
-        post :create, params: valid_attrs
+        post :create, params: { toy: valid_attrs }
         expect(response).to redirect_to toys_path
       end
     end
@@ -41,7 +49,7 @@ RSpec.describe ToysController, type: :controller do
     context "with invalid params" do
       context "when model rejects attrs" do
         it "returns success to render 'new'" do
-          post :create, params: invalid_attrs
+          post :create, params: { toy: invalid_attrs }
           expect(response).to be_successful
         end
       end
