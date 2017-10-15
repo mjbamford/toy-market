cc18f39dac5f08ca4f36fd89bec0b95ef5e967a1
Michael Bamford <michael@givvtechnologies.com>
2017-10-15 17:04:47 +1100

    Add message association to User

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,21 @@
 class MessagesController < ApplicationController
+  MESSAGE_PERMITTED_PARAMS = %i[ recipient_id text ]
+
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
+    @message = Message.find params[:id]
   end
 
   def new
-    @message = Message.new sender: current_user, recipient: current_user
+    @message = current_user.sent_messages.new recipient: current_user
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.sent_messages.new message_params
+
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
@@ -24,20 +27,9 @@ class MessagesController < ApplicationController
     end
   end
 
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def destroy
-    @message = Message.find(params[:id])
+    @message = Message.find params[:id]
+
     @message.destroy
     respond_to do |format|
       format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
@@ -48,6 +40,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:sender_id, :recipient_id, :text)
+    params.require(:message).permit MESSAGE_PERMITTED_PARMS
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :toys_to_sell, class_name: 'Toy', foreign_key: 'seller_id'
   has_many :toys_to_buy,  class_name: 'Toy', foreign_key: 'buyer_id'
+
+  has_many :sent_messages,     class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
 end
