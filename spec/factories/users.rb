e28d74e5ebbbad80a665d1c5cccdef302f891981
Michael Bamford <michael@givvtechnologies.com>
2017-10-17 15:13:18 +1100

    Add seeds to development

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,15 @@ FactoryGirl.define do
   factory :user do
   end
 
-  factory :seller, parent: :user do
+  factory :seller, aliases: [ :sallyseller ], parent: :user do
+    username   :sallyseller
+    first_name :Sally
+    last_name  :Seller
+  end
+
+  factory :buyer, aliases: [ :barrybuyer ], parent: :user do
+    username   :barrybuyer
+    first_name :Barry
+    last_name  :Buyer
   end
 end
