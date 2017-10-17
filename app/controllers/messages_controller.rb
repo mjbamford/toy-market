dcc9d8d68f2a13561ae42abe71d8fd7411e02869
Michael Bamford <michael@givvtechnologies.com>
2017-10-17 22:52:48 +1100

    Format messages#show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,7 +17,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.sent_messages.new message_params
-
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
