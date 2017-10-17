a73be6ab2f68be4cbfc8af11a3ecf08f43531d35
Michael Bamford <michael@givvtechnologies.com>
2017-10-17 15:16:38 +1100

    Remove crud from request, helper message specs

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Messages", type: :request do
-  describe "GET /messages" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Messages', type: :request do
+  describe 'GET /messages' do
+    it 'returns returns status 200' do
       get messages_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status 200
     end
   end
 end
