b0be5d721c56630ea832665bff2e70163b596317
Michael Bamford <michael@givvtechnologies.com>
2017-10-18 07:35:53 +1100

    Add ApplicationHelper#current_user

--- spec/views/toys/_toy.html.haml_spec.rb
+++ spec/views/toys/_toy.html.haml_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'toys/_toy', type: :view do
   let!(:toy) { create :toy }
+  let(:seller) { create :seller }
 
   it "matches 'Toys'" do
+    allow(view).to receive(:current_user).and_return seller
     render 'toys/toy', toy: toy
     assert_select 'div[id=?]', (dom_id toy)
   end
